9d53b59e658b0896b59725ba8b7ec41eb13e328f
phauer <info@philipphauer.de>
2014-12-20 21:12:36 +0100

    small refactoring

--- svgexodus/src/main/java/de/philipphauer/svgexodus/io/IOUtil.java
+++ svgexodus/src/main/java/de/philipphauer/svgexodus/io/IOUtil.java
@@ -31,114 +31,108 @@ import de.philipphauer.svgexodus.gui.GuiConst;
  */
 public final class IOUtil {
 
-    private static final Logger logger = LoggerFactory.getLogger(IOUtil.class);
+	private static final Logger logger = LoggerFactory.getLogger(IOUtil.class);
 
-    /**
-     * Läuft rekursiv alle Unterordner ab und gibt Liste von SVG-Dateien zurück.
-     */
-    public static List<Path> getAllSVGFiles(Path path) {
-        final ArrayList<Path> pathList = new ArrayList<>();
-        try {
-            Files.walkFileTree(path, new FileVisitorAdapter<Path>() {
+	/**
+	 * Läuft rekursiv alle Unterordner ab und gibt Liste von SVG-Dateien zurück.
+	 */
+	public static List<Path> getAllSVGFiles(Path path) {
+		final List<Path> pathList = new ArrayList<>();
+		try {
+			Files.walkFileTree(path, new FileVisitorAdapter<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					if (attrs.isRegularFile() && file.toString().endsWith(".svg")) {
+						pathList.add(file);
+					}
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {
+			String message = "Error while searching through the file system.";
+			JOptionPane.showMessageDialog(null, message + GuiConst.LINE_SEPARATOR + e.getMessage());
+			logger.error(message, e);
+		}
+		return pathList;
+	}
 
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    if (attrs.isRegularFile() && file.toString().endsWith(".svg")) {
-                        pathList.add(file);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
-        catch (IOException e) {
-            String message = "Error while searching through the file system.";
-            JOptionPane.showMessageDialog(null, message + GuiConst.LINE_SEPARATOR + e.getMessage());
-            logger.error(message, e);
-        }
-        return pathList;
-    }
+	public static List<Path> getAllSubDirectorys(Path pBaseDir) {
+		final List<Path> pathList = new ArrayList<>();
+		try {
+			Files.walkFileTree(pBaseDir, new FileVisitorAdapter<Path>() {
+				@Override
+				public FileVisitResult preVisitDirectory(Path pDir, BasicFileAttributes pAttrs) throws IOException {
+					pathList.add(pDir);
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} catch (IOException e) {
+			String message = "Error while searching through the file system.";
+			JOptionPane.showMessageDialog(null, message + GuiConst.LINE_SEPARATOR + e.getMessage());
+			logger.error(message, e);
+		}
+		return pathList;
+	}
 
-    public static List<Path> getAllSubDirectorys(Path pBaseDir) {
-        final ArrayList<Path> pathList = new ArrayList<>();
-        try {
-            Files.walkFileTree(pBaseDir, new FileVisitorAdapter<Path>() {
+	private static final Map<Path, Date> cMap = new HashMap<>();
 
-                @Override
-                public FileVisitResult preVisitDirectory(Path pDir, BasicFileAttributes pAttrs) throws IOException {
-                    pathList.add(pDir);
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        }
-        catch (IOException e) {
-            String message = "Error while searching through the file system.";
-            JOptionPane.showMessageDialog(null, message + GuiConst.LINE_SEPARATOR + e.getMessage());
-            logger.error(message, e);
-        }
-        return pathList;
-    }
+	private static final int THRESHOLD_OBSERVING_TIME_ELAPSED_FOR_VALID_EVENT = 4500;
 
-    private static final Map<Path, Date> cMap = new HashMap<>();
+	/**
+	 * Kontext: Der WatchService erzeugt bei Änderungen nicht nur ein Event, sondern häufig zwei direkt hintereinander.
+	 * Da das Problem nicht zu lösen war (bzw. es sein kann, dass der Editor wirklich zweimal die Datei modifziert),
+	 * wird eine Heuristik verwendet. Ein Event (ausgelöst von eine Änderung auf einem Path) ist dann gültig, wenn das
+	 * letzte Event auf den selben Path mehr als -THRESHOLD_OBSERVING_TIME_ELAPSED_FOR_VALID_EVENT- Sekunden her ist.
+	 * --> Schwellwert
+	 */
+	public static boolean isEventValid(Path pPath) {
+		Date timeOfCurrentEvent = new Date();
+		Date timeofLastEvent = cMap.get(pPath);
+		boolean isThresholdExceeded = (timeOfCurrentEvent.getTime() - timeofLastEvent.getTime()) > THRESHOLD_OBSERVING_TIME_ELAPSED_FOR_VALID_EVENT;
+		if (timeofLastEvent == null || isThresholdExceeded) {
+			cMap.put(pPath, timeOfCurrentEvent);
+			return true;
+		}
+		return false;
+	}
 
-    private static final int THRESHOLD_OBSERVING_TIME_ELAPSED_FOR_VALID_EVENT = 4500;
+	public static String detectCharset(Path pSvgFile) throws IOException {
+		byte[] buf = new byte[4096];
+		try (FileInputStream fis = new FileInputStream(pSvgFile.toFile())) {
+			UniversalDetector detector = new UniversalDetector(null);
 
-    /**
-     * Kontext: Der WatchService erzeugt bei Änderungen nicht nur ein Event, sondern häufig zwei direkt hintereinander.
-     * Da das Problem nicht zu lösen war (bzw. es sein kann, dass der Editor wirklich zweimal die Datei modifziert), wird eine 
-     * Heuristik verwendet. Ein Event (ausgelöst von eine Änderung auf einem Path) ist dann gültig, wenn das letzte
-     * Event auf den selben Path mehr als -THRESHOLD_OBSERVING_TIME_ELAPSED_FOR_VALID_EVENT- Sekunden her ist. --> Schwellwert
-     */
-    public static boolean isEventValid(Path pPath) {
-        Date timeOfCurrentEvent = new Date();
-        Date timeofLastEvent = cMap.get(pPath);
-        if (timeofLastEvent == null
-                || (timeOfCurrentEvent.getTime() - timeofLastEvent.getTime()) > THRESHOLD_OBSERVING_TIME_ELAPSED_FOR_VALID_EVENT) {
-            //mehr als eine sec vergangen --> ok
-            cMap.put(pPath, timeOfCurrentEvent);
-            return true;
-        }
-        return false;
-    }
+			int nread;
+			while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
+				detector.handleData(buf, 0, nread);
+			}
+			detector.dataEnd();
 
-    public static String detectCharset(Path pSvgFile) throws IOException {
-        byte[] buf = new byte[4096];
-        try (FileInputStream fis = new FileInputStream(pSvgFile.toFile())) {
-            UniversalDetector detector = new UniversalDetector(null);
+			String encoding = detector.getDetectedCharset();
 
-            int nread;
-            while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
-                detector.handleData(buf, 0, nread);
-            }
-            detector.dataEnd();
+			detector.reset();
+			return encoding;
+		}
+	}
 
-            String encoding = detector.getDetectedCharset();
-            //null, wenn nicht erfolgreich
+	public static void openLogFile() {
+		try {
+			String logFilePath = getLogFilePath();
+			Desktop.getDesktop().open(new File(logFilePath));
+		} catch (IOException e) {
+			String message = "Couln't open log file: " + e.getMessage();
+			JOptionPane.showMessageDialog(null, message);
+			logger.error(message, e);
+		}
 
-            detector.reset();
-            return encoding;
-        }
-    }
+	}
 
-    public static void openLogFile() {
-        try {
-            String logFilePath = getLogFilePath();
-            Desktop.getDesktop().open(new File(logFilePath));
-        }
-        catch (IOException e) {
-            String message = "Couln't open log file: " + e.getMessage();
-            JOptionPane.showMessageDialog(null, message);
-            logger.error(message, e);
-        }
-
-    }
-
-    private static String getLogFilePath() throws FileNotFoundException, IOException {
-        Properties logProps = new Properties();
-        InputStream stream = Main.class.getResourceAsStream("/log4j.properties");
-        logProps.load(stream);
-        String logFilePath = logProps.getProperty("log4j.appender.file.File");
-        logFilePath = logFilePath.replace("${java.io.tmpdir}/", System.getProperty("java.io.tmpdir"));
-        return logFilePath;
-    }
+	private static String getLogFilePath() throws FileNotFoundException, IOException {
+		Properties logProps = new Properties();
+		InputStream stream = Main.class.getResourceAsStream("/log4j.properties");
+		logProps.load(stream);
+		String logFilePath = logProps.getProperty("log4j.appender.file.File");
+		logFilePath = logFilePath.replace("${java.io.tmpdir}/", System.getProperty("java.io.tmpdir"));
+		return logFilePath;
+	}
 
 }
